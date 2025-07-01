69f09a15a98ad93df0be6751739e0413bc95e9e4
KYNAY <88751526+KYNAY@users.noreply.github.com>
2025-06-30 22:44:16 -0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'Roboto, sans-serif' }}>
-      <h1>Bem-vindo à Calculadora de Remuneração</h1>
-      <Link href="/pagamento"><a style={{ color: '#6200ee' }}>Ir para a Calculadora</a></Link>
-    </div>
-  );
-}
